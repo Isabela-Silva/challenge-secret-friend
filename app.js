8092408a4de7fe74399d1eb09ef1c65045d6ca82
isadev <isareginabs@gmail.com>
2025-01-18 17:18:39 -0300

    project done!

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaFriends = [];
+
+
+
+
+function adicionarAmigo() {
+    let nameFriend = document.querySelector('input').value
+    let listaAmigosTela = document.getElementById('listaAmigos')
+
+    listaFriends.push(nameFriend)
+    clean();
+
+    listaAmigosTela.innerHTML = listaFriends
+
+
+}
+
+function clean() {
+    let nameFriend = document.querySelector('input')
+    nameFriend.value = '';
+
+}
+
+function sortearAmigo() {
+    amigoSorteado = listaFriends[Math.floor(Math.random() * listaFriends.length)]
+
+
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML = amigoSorteado
+
+}
